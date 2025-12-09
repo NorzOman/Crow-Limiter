a7b2be53e0bdbe1ceabae114cf2769729372154e
Norz Oman <oman@oman.oman>
2025-12-09 17:08:32 +0530

    removed unwwanted pushed stuffs

--- test.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <ctime>
-#include <iostream>
-
-int main() {
-    std::time_t now = std::time(nullptr);  // current epoch time in seconds
-    std::cout << "Now: " << now << std::endl;
-    return 0;
-}
